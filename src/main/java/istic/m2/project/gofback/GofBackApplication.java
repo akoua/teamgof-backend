6ea6c020b3f66833831bba38d129ea771692b27b
akoua <akouayao225@gmail.com>
2023-05-04 08:49:48 +0200

    add_init_configuration

---
+++ src/main/java/istic/m2/project/gofback/GofBackApplication.java
@@ -0,0 +1,13 @@
+package istic.m2.project.gofback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GofBackApplication.class, args);
+    }
+
+}
