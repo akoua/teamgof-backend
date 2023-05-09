6c7d929f6f0f8041639839c71f4a1f00b930bdbf
akoua <akouayao225@gmail.com>
2023-05-09 08:49:02 +0200

    change_database_url

--- src/main/resources/db/migration/V0/V0.0.0__init_database.sql
+++ src/main/resources/db/migration/V0/V0.0.0__init_database.sql
@@ -1,8 +1,8 @@
-CREATE SCHEMA gof_team;
+CREATE SCHEMA IF NOT EXISTS gof_team;
 
 SET search_path TO gof_team;
 
-CREATE TABLE cavalier
+CREATE TABLE IF NOT EXISTS cavalier
 (
     cavalier_id        BIGINT                                    NOT NULL,
     first_name         VARCHAR(255),
@@ -17,7 +17,7 @@ CREATE TABLE cavalier
     CONSTRAINT pk_cavalier PRIMARY KEY (cavalier_id)
 );
 
-CREATE TABLE discipline
+CREATE TABLE IF NOT EXISTS discipline
 (
     discipline_id      BIGINT                                    NOT NULL,
     name               VARCHAR(255),
@@ -28,7 +28,7 @@ CREATE TABLE discipline
     CONSTRAINT pk_discipline PRIMARY KEY (discipline_id)
 );
 
-CREATE TABLE poney
+CREATE TABLE IF NOT EXISTS poney
 (
     poney_id           BIGINT                                    NOT NULL,
     cavalier_id        BIGINT,
@@ -39,10 +39,10 @@ CREATE TABLE poney
     CONSTRAINT pk_poney PRIMARY KEY (poney_id)
 );
 
-ALTER TABLE poney
+ALTER TABLE IF EXISTS  poney
     ADD CONSTRAINT FK_PONEY_ON_CAVALIER FOREIGN KEY (cavalier_id) REFERENCES cavalier (cavalier_id);
 
-CREATE TABLE cavalier_team_participated
+CREATE TABLE IF NOT EXISTS cavalier_team_participated
 (
     cavalier_team_participated_id BIGINT                                    NOT NULL,
     created_by                    VARCHAR(255),
@@ -54,7 +54,7 @@ CREATE TABLE cavalier_team_participated
     CONSTRAINT pk_cavalier_team_participated PRIMARY KEY (cavalier_team_participated_id)
 );
 
-CREATE TABLE epreuve_team_participated
+CREATE TABLE IF NOT EXISTS epreuve_team_participated
 (
     epreuve_team_participated_id BIGINT                                    NOT NULL,
     created_by                   VARCHAR(255),
@@ -66,7 +66,7 @@ CREATE TABLE epreuve_team_participated
     CONSTRAINT pk_epreuve_team_participated PRIMARY KEY (epreuve_team_participated_id)
 );
 
-CREATE TABLE cavalier_epreuve_participated
+CREATE TABLE IF NOT EXISTS cavalier_epreuve_participated
 (
     cavalier_epreuve_participated_id BIGINT                                    NOT NULL,
     created_by                       VARCHAR(255),
@@ -78,7 +78,7 @@ CREATE TABLE cavalier_epreuve_participated
     CONSTRAINT pk_cavalier_epreuve_participated PRIMARY KEY (cavalier_epreuve_participated_id)
 );
 
-CREATE TABLE cavalier_epreuve_practice
+CREATE TABLE IF NOT EXISTS cavalier_epreuve_practice
 (
     cavalier_epreuve_practice_id BIGINT                                    NOT NULL,
     created_by                   VARCHAR(255),
@@ -92,7 +92,7 @@ CREATE TABLE cavalier_epreuve_practice
     CONSTRAINT pk_cavalier_epreuve_practice PRIMARY KEY (cavalier_epreuve_practice_id)
 );
 
-CREATE TABLE epreuve
+CREATE TABLE IF NOT EXISTS epreuve
 (
     epreuve_id         BIGINT                                    NOT NULL,
     discipline_id      BIGINT,
@@ -105,23 +105,23 @@ CREATE TABLE epreuve
     CONSTRAINT pk_epreuve PRIMARY KEY (epreuve_id)
 );
 
-ALTER TABLE cavalier_epreuve_participated
+ALTER TABLE IF EXISTS  cavalier_epreuve_participated
     ADD CONSTRAINT FK_CAVALIER_EPREUVE_PARTICIPATED_ON_CAVALIER FOREIGN KEY (cavalier_id) REFERENCES cavalier (cavalier_id);
 
-ALTER TABLE cavalier_epreuve_participated
+ALTER TABLE IF EXISTS  cavalier_epreuve_participated
     ADD CONSTRAINT FK_CAVALIER_EPREUVE_PARTICIPATED_ON_EPREUVE FOREIGN KEY (epreuve_id) REFERENCES epreuve (epreuve_id);
 
-ALTER TABLE cavalier_epreuve_practice
+ALTER TABLE IF EXISTS  cavalier_epreuve_practice
     ADD CONSTRAINT FK_CAVALIER_EPREUVE_PRACTICE_ON_CAVALIER FOREIGN KEY (cavalier_id) REFERENCES cavalier (cavalier_id);
 
-ALTER TABLE cavalier_epreuve_practice
+ALTER TABLE IF EXISTS  cavalier_epreuve_practice
     ADD CONSTRAINT FK_CAVALIER_EPREUVE_PRACTICE_ON_EPREUVE FOREIGN KEY (epreuve_id) REFERENCES epreuve (epreuve_id);
 
-ALTER TABLE epreuve
+ALTER TABLE IF EXISTS  epreuve
     ADD CONSTRAINT FK_EPREUVE_ON_DISCIPLINE FOREIGN KEY (discipline_id) REFERENCES discipline (discipline_id);
 
 
-CREATE TABLE team
+CREATE TABLE IF NOT EXISTS team
 (
     team_id            BIGINT                                    NOT NULL,
     created_by         VARCHAR(255),
@@ -131,14 +131,14 @@ CREATE TABLE team
     CONSTRAINT pk_team PRIMARY KEY (team_id)
 );
 
-ALTER TABLE cavalier_team_participated
+ALTER TABLE IF EXISTS  cavalier_team_participated
     ADD CONSTRAINT FK_CAVALIER_TEAM_PARTICIPATED_ON_CAVALIER FOREIGN KEY (cavalier_id) REFERENCES cavalier (cavalier_id);
 
-ALTER TABLE cavalier_team_participated
+ALTER TABLE IF EXISTS  cavalier_team_participated
     ADD CONSTRAINT FK_CAVALIER_TEAM_PARTICIPATED_ON_TEAM FOREIGN KEY (team_id) REFERENCES team (team_id);
 
-ALTER TABLE epreuve_team_participated
+ALTER TABLE IF EXISTS  epreuve_team_participated
     ADD CONSTRAINT FK_EPREUVE_TEAM_PARTICIPATED_ON_EPREUVE FOREIGN KEY (epreuve_id) REFERENCES epreuve (epreuve_id);
 
-ALTER TABLE epreuve_team_participated
+ALTER TABLE IF EXISTS  epreuve_team_participated
     ADD CONSTRAINT FK_EPREUVE_TEAM_PARTICIPATED_ON_TEAM FOREIGN KEY (team_id) REFERENCES team (team_id);
